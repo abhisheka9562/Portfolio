42a46b3a6c65e493954fe9c97487e687e31ba83f
Abhishek A <abhisheka9562@email.com>
2025-06-12 15:28:54 +0530

    Styling:Edited about section style

--- script.js
+++ script.js
@@ -16,35 +16,4 @@ function openTab(tabId) {
   menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show');
   });
-  // form 
-  // const scriptURL = 'https://script.google.com/macros/s/AKfycbx4a-rmJoEitUzJNPXVWZV1HGDqGle9nKnTyNC3mi-i9GGPwR8ErqxwZB0sZ-uM85ej/exec';
-  // const form = document.getElementById('contact-form');
-
-  // form.addEventListener('submit', e => {
-  //   e.preventDefault();
-  //   const formData = new FormData(form);
-  //   const data = {
-  //     name: formData.get('name'),
-  //     email: formData.get('email'),
-  //     message: formData.get('message')
-  //   };
-
-  //   fetch(scriptURL, {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify(data)
-  //   })
-  //   .then(response => {
-  //     if (response.ok) {
-  //       alert("Message sent successfully!");
-  //       form.reset();
-  //     } else {
-  //       alert("Server error. Please try again.");
-  //     }
-  //   })
-  //   .catch(error => {
-  //     console.error("Error!", error.message);
-  //     alert("Error sending message.");
-  //   });
-  // });
 
